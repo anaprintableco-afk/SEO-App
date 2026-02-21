e5ebcfb6c2ec9def7fa8265649fe904aef48e49b
anaprintableco-afk <ana.printable.co@gmail.com>
2026-02-21 15:44:47 +0330

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,8 +1,17 @@
 # constants.py
+
+# لیست کامل رنگ‌ها
 ALLOWED_COLORS = ["Beige", "Black", "Blue", "Bronze", "Brown", "Clear", "Copper", "Gold", "Grey", "Green", "Orange", "Pink", "Purple", "Rainbow", "Red", "Rose gold", "Silver", "White", "Yellow"]
+
+# استایل‌های هنری دکوراسیون
 ALLOWED_STYLES = ["Art deco", "Art nouveau", "Bohemian & eclectic", "Coastal & tropical", "Contemporary", "Country & farmhouse", "Gothic", "Industrial & utility", "Lodge", "Mid-century", "Minimalist", "Rustic & primitive", "Southwestern", "Victorian"]
-ALLOWED_CELEBS = ["Christmas", "Cinco de Mayo", "Easter", "Halloween", "Mother's Day", "New Year's", "Thanksgiving", "Valentine's Day"] # خلاصه شده
-ALLOWED_OCCASIONS = ["Anniversary", "Birthday", "Graduation", "Housewarming", "Wedding"] # خلاصه شده
-ALLOWED_SUBJECTS = ["Abstract & geometric", "Animal", "Architecture & cityscape", "Flowers", "Landscape & scenery", "Minimalist", "Plants & trees"]
-ALLOWED_ROOMS = ["Bathroom", "Bedroom", "Entryway", "Game room", "Kids", "Kitchen & dining", "Living room", "Nursery", "Office"]
-FORBIDDEN_TV_WORDS = ["print", "printable", "poster", "canvas", "wall decor", "physical", "download", "instant"]
+
+# موضوعات هنری (Subject) - این لیست رو خیلی بزرگتر کردیم
+ALLOWED_SUBJECTS = ["Abstract & geometric", "Animal", "Anime & cartoon", "Architecture & cityscape", "Beach & tropical", "Botanical", "Comics & manga", "Educational", "Fantasy & Sci Fi", "Fashion", "Flowers", "Food & drink", "Geography & locale", "Horror & gothic", "Humourous saying", "Landscape & scenery", "LGBTQ pride", "Love & friendship", "Military", "Music", "Nautical", "People & portrait", "Plants & trees", "Religious", "Science & tech", "Sports & fitness", "Stars & celestial", "Travel & transportation", "Typography & symbols", "Video game", "Western & cowboy", "Zodiac"]
+
+# اتاق‌های هدف
+ALLOWED_ROOMS = ["Bathroom", "Bedroom", "Dorm", "Entryway", "Game room", "Kids", "Kitchen & dining", "Laundry", "Living room", "Nursery", "Office"]
+
+# مناسبت‌ها و جشن‌ها (Occasions & Celebrations)
+ALLOWED_CELEBRATIONS = ["None", "Christmas", "Cinco de Mayo", "Easter", "Eid", "Father's Day", "Halloween", "Hanukkah", "Mother's Day", "New Year's", "Passover", "Ramadan", "St Patrick's Day", "Thanksgiving", "Valentine's Day"]
+ALLOWED_OCCASIONS = ["None", "Anniversary", "Baby shower", "Birthday", "Bridal shower", "Confirmation", "Engagement", "Graduation", "Housewarming", "Retirement", "Wedding"]
