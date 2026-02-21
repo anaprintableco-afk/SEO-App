764ca94177b4fe26747d99ab4d80995b61abdcca
anaprintableco-afk <ana.printable.co@gmail.com>
2026-02-21 13:14:55 +0330

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+# constants.py
+ALLOWED_COLORS = ["Beige", "Black", "Blue", "Bronze", "Brown", "Clear", "Copper", "Gold", "Grey", "Green", "Orange", "Pink", "Purple", "Rainbow", "Red", "Rose gold", "Silver", "White", "Yellow"]
+ALLOWED_STYLES = ["Art deco", "Art nouveau", "Bohemian & eclectic", "Coastal & tropical", "Contemporary", "Country & farmhouse", "Gothic", "Industrial & utility", "Lodge", "Mid-century", "Minimalist", "Rustic & primitive", "Southwestern", "Victorian"]
+ALLOWED_CELEBS = ["Christmas", "Cinco de Mayo", "Easter", "Halloween", "Mother's Day", "New Year's", "Thanksgiving", "Valentine's Day"] # خلاصه شده
+ALLOWED_OCCASIONS = ["Anniversary", "Birthday", "Graduation", "Housewarming", "Wedding"] # خلاصه شده
+ALLOWED_SUBJECTS = ["Abstract & geometric", "Animal", "Architecture & cityscape", "Flowers", "Landscape & scenery", "Minimalist", "Plants & trees"]
+ALLOWED_ROOMS = ["Bathroom", "Bedroom", "Entryway", "Game room", "Kids", "Kitchen & dining", "Living room", "Nursery", "Office"]
+FORBIDDEN_TV_WORDS = ["print", "printable", "poster", "canvas", "wall decor", "physical", "download", "instant"]
